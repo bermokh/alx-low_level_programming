723dbc9fb5ed75a0236874ac48e5d58c930a58f1
mokhtara <you@example.com>
2023-04-25 12:30:12 -0700

    function into function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* Description: print_alphabet - prints the alphabet, in lowercase
+*
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+char alpha;
+for (alpha = 97; alpha <= 122; alpha++)
+{
+_putchar(alpha);
+_putchar('\n');
+}
+}
+int main(void)
+{
+void print_alphabet(void);
+return (0);
+}
