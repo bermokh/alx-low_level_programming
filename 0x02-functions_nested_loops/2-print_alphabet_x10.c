60258956e1f59452c70b5f54a2498347e4ed9b79
mokhtara <you@example.com>
2023-04-25 14:23:31 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* Description: print_alphabet_x10 - print alphabet ten time.
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 1; i <= 10; i++)
+{
+void print_alphabet(void);
+}
+}
