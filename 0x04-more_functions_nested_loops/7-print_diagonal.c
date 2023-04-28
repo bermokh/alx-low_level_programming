0f772865ed449610dfa8eff32bf953837fdca6f6
mokhtara <you@example.com>
2023-04-28 10:12:11 -0700

    #

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,22 +5,27 @@
 */
 void print_diagonal(int n)
 {
-int x, y;
 if (n <= 0)
 {
 _putchar('\n');
-}
+} 
 else
 {
-for (x = 0; x < n; x++)
+int i, j;
+for (i = 0; i < n; i++)
 {
-for (y = 0; y < n; y++)
+for (j = 0; j < n; j++)
 {
-_putchar(32);
+if (j == i)
+{
+_putchar('\\');
 }
-_putchar(92);
+else if (j < i)
+{
+_putchar(' ');
+}
 _putchar('\n');
 }
 }
 }
-
+}
