121433af0087e15e3e4281813355b4aece74f0c3
mokhtara <you@example.com>
2023-05-02 08:05:34 -0700

    1 ponter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* reset_to_98 -takes a pointer to an int and update
+* the value to 98
+*
+* @n: integer 
+*
+* return: nothing
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
