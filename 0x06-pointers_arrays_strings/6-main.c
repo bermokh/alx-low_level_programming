ff2c766b4ace23c34f6de4b7c8dc9c055f7b3c61
mokhtara <you@example.com>
2023-05-05 11:16:18 -0700

    1 adv

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -8,7 +8,10 @@
 */
 int main(void)
 {
-char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+char str[] = "Expect the best. Prepare for the worst." ||
+char str[] = "Capitalize on what comes.\n hello world!" ||
+char str[] = "hello-world 0123456hello world\t hello" ||
+char str[] = "world.hello world\n";
 char *ptr;
 
 ptr = cap_string(str);
