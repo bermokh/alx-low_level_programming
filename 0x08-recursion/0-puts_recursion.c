d31b445da04eb930af5ce46806ed19033ccf604f
mokhtara <you@example.com>
2023-05-10 03:19:04 -0700

    correction

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,13 +2,14 @@
 /**
 *_puts_recursion -  function that prints a string, followed by a new line.
 *@s: the string to print
-*
+*Return: nothing to give
 */
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
-_putchar('\0');
+_putchar('\n');
+return;
 }
 else
 {
