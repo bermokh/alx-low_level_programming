4edb3d14f86291466d8dd6faeccec5b9c1e99c5a
mokhtara <you@example.com>
2023-05-16 14:15:15 -0700

    appdate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,21 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*_strlen - find a lenght of sting
-*@s: string ussing
-*Return: the lenght of string
-*/
-int _strlen(char *s)
-{
-int i = 0;
-while (*s != '\0')
-{
-i++;
-s++;
-}
-return (i);
-}
-/**
 **str_concat - function that concatenates two strings
 *@s1: the first string
 *@s2: the second string
@@ -24,24 +9,32 @@ return (i);
 */
 char *str_concat(char *s1, char *s2)
 {
-int size1, size2, i;
-char *p;
-if (s2 == NULL)
+char *s;
+int i = 0, j = 0, k = 0, l = 0;
+if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-size1 = _strlen(s1);
-size2 = _strlen(s2);
-p = malloc((size1 + size2) *sizeof(char) + 1);
-if (p == 0)
-return (0);
-for (i = 0; i <= size1 + size2; i++)
+while (s1[i])
+i++;
+while (s2[j])
+j++;
+l = i + j;
+s = malloc((sizeof(char) * l) +1);
+if (s == NULL)
+return (NULL);
+j = 0;
+while (k < l)
 {
-if (i < size1)
-p[i] = s1[i];
-else
-p[i] = s2[i - size1];
+if (k <= i)
+s[k] = s1[k];
+if (k >= i)
+{
+s[k] = s2[j];
+j++;
 }
-p[i] = '\0';
-return (p);
+k++;
+}
+s[k] = '\0';
+return (s);
 }
