10bdfa9cb6c5064c1c09e061ede824d80346e375
mokhtara <you@example.com>
2023-05-22 05:14:04 -0700

    my first structer

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - a data type
+*@name: the first structer members
+*@age: the second structer members of type lf
+*@owner: the last structer members
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
