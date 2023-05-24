1af8b6efbf6af77960563a72200d193372a21e9b
mokhtara <you@example.com>
2023-05-24 07:14:44 -0700

    show us the array using a function poiter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+*array_iterator - function that executes a function given
+*as a parameter on each element of an array.
+*@array: the array using in function main
+*@size: is the size of the array
+*@action: is a pointer to the function you need to use
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+if (array == NULL && action == NULL)
+{
+return;
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
+}
