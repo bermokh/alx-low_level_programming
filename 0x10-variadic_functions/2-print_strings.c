3f81392c939fb525705b8395cff14b04918f1f4a
mokhtara <you@example.com>
2023-05-25 13:46:47 -0700

    corret

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,19 +17,19 @@ va_start(l, n);
 for (i = 0; i < n ; i++)
 {
 ptr = va_arg(l, char*);
-if (ptr != NULL)
-{
-printf("%s", ptr);
-}
-else
+if (ptr == NULL)
 {
 printf("nil");
 }
-if (separator != NULL && i < n - 1)
+else
+{
+printf("%s", ptr);
+}
+if (separator != NULL && i != n - 1)
 {
 printf("%s", separator);
 }
 }
-va_end(l);
 printf("\n");
+va_end(l);
 }
