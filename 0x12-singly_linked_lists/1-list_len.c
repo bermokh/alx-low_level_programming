24c1ea9a250b652cc6cf27047fa31cc52143a7ba
bermokh <berramimokhtara@gmail.com>
2023-06-02 13:26:42 -0700

    count the number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+*list_len - function that returns the number of elements in a linked list_t
+*@h: the pointer of the first nod
+*Return: the number of element
+*/
+size_t list_len(const list_t *h)
+{
+int count = 0;
+while (h != NULL)
+{
+h = (*h).next;
+count++;
+}
+return (count);
+}
