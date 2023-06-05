290fc14bd67b59ae869cd26cb975cb9e2402b02b
bermokh <berramimokhtara@gmail.com>
2023-06-05 14:53:21 -0700

    count the number of elements

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,9 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-*listint_len - function that returns the number of elements in a linked listint_t list.
-*@h: the pointer of the first nod 
-*Return:  the number of elements 
+*listint_len - function that returns the number
+*of elements in a linked listint_t list.
+*@h: the pointer of the first nod
+*Return:  the number of elements
 */
 size_t listint_len(const listint_t *h)
-
+{
+int i = 0;
+while (h != NULL)
+{
+i++;
+h = (*h).next;
+}
+return (i);
+}
