7fe9764262beb87b97278489e2499b37e0264af6
bermokh <berramimokhtara@gmail.com>
2023-06-05 15:09:17 -0700

    add a nod on the begnning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,10 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-*add_nodeint - function that adds a new node at the beginning of a listint_t list.
-*head: the first pointer of the first liste
+*add_nodeint - function that adds a new
+*node at the beginning of a listint_t list.
+*@head: the first pointer of the first liste
 *@n: the data of the new list
 *Return: the address of the new element
 */
 listint_t *add_nodeint(listint_t **head, const int n)
-
+{
+listint_t *p;
+p = malloc(sizeof(listint_t));
+if (!p)
+return (NULL);
+else
+(*p).n = n;
+(*p).next = *head;
+*head = p;
+return (p);
+}
