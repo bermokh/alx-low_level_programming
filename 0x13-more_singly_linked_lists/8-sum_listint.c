6da165aa590b2cae3e2521d77c9c59188dcf8efc
bermokh <berramimokhtara@gmail.com>
2023-06-06 08:15:53 -0700

    sum of all data list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,8 +3,20 @@
 #include <stdio.h>
 #include "lists.h"
 /**
-*sum_listint -  function that returns the sum of all the data (n) of a listint_t linked list.
+*sum_listint -  function that returns the sum
+*of all the data (n) of a listint_t linked list.
 *@head: pointres of the first data
 *Return:  the sum of all the data
 */
 int sum_listint(listint_t *head)
+{
+unsigned int sum = 0;
+if (head == NULL)
+return (0);
+while (head != NULL)
+{
+sum += (*head).n;
+head = (*head).next;
+}
+return (sum);
+}
