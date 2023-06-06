dbe4aa51d34c738c937e0db9719401089586a25c
bermokh <berramimokhtara@gmail.com>
2023-06-06 04:09:08 -0700

    update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 #include "lists.h"
 /**
-*get_nodeint_at_index - function that returns the nth node of a listint_t linked list.
+*get_nodeint_at_index - function that returns
+*the nth node of a listint_t linked list.
 *@head: the pointer of the data
 *@index: the number of data
 *Return: the nth nod
