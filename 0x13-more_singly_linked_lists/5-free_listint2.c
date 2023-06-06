d76599ba2e93aeda32f9faa2fd8cb2f778a1a65c
bermokh <berramimokhtara@gmail.com>
2023-06-06 03:24:05 -0700

    free a list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,3 +6,15 @@
 *@head: pointre of the node
 */
 void free_listint2(listint_t **head)
+{
+listint_t *ptr;
+if (head == NULL)
+return;
+while (*head != NULL)
+{
+ptr = (*head)->next;
+free(*head);
+*head = ptr;
+}
+*head = NULL;
+}
