46439384cf1d7997ad8fe0ef70f2cb5839a1a521
bermokh <berramimokhtara@gmail.com>
2023-06-08 06:41:43 -0700

    bit of index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,3 +6,10 @@
 *Return: the value of a bit at a given index
 */
 int get_bit(unsigned long int n, unsigned int index)
+{
+int bit;
+if (index > 63)
+return (-1);
+bit = (n >> index) & 1;
+return (bit);
+}
