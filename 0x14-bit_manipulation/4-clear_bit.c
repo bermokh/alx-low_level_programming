b9f212f8efc44692af7353f77774ce4742dc3e89
bermokh <berramimokhtara@gmail.com>
2023-06-08 06:47:31 -0700

    sets the value of a bit to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,3 +6,9 @@
 *Return: 1 if it worked, or -1 if an error occurred
 */
 int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index > 63)
+return (-1);
+*n = (~(1UL << index) & *n);
+return (1);
+}
