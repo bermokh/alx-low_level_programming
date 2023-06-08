770b54d09346b0705d655a6dc2802cb32d10f7e4
bermokh <berramimokhtara@gmail.com>
2023-06-08 06:38:52 -0700

    covesrt to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,3 +4,20 @@
 *@n: the integere numbre
 */
 void print_binary(unsigned long int n)
+{
+int i, j = 0;
+unsigned long int result;
+for (i = 63; i >= 0; i--)
+{
+result = n >> i;
+if (result & 1)
+{
+_putchar('1');
+j++;
+}
+else if (j)
+_putchar('0');
+}
+if (!j)
+_putchar('0');
+}
