ee9cc75476cb8ebc4c80c2223b16fb153a871ce7
bermokh <berramimokhtara@gmail.com>
2023-06-16 03:58:39 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
-*read_textfile - function that reads a text file and prints it to the POSIX standard output.
-*@filename: the name of current file 
+*read_textfile - function that reads a text file and
+*prints it to the POSIX standard output.
+*@filename: the name of current file
 *@letters: is the number of letters it should read and print
 *Return: the actual number of letters it could read and print
 */
