d60b09ba23dfa1e45a9c0cc9c50ac9cbd661c413
berrami mokhtara <berramimokhtara@gmail.com>
2023-07-10 16:13:04 -0700

    up3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ if (filename == NULL)
 return (-1);
 if (text_content != NULL)
 {
-for (len = 0; text_content[len]; len++)
+for (len = 0; text_content[len];)
 len++;
 }
 o = open(filename, O_WRONLY | O_APPEND);
