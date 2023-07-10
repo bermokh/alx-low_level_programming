6ec3b116500f845af4bb549d32f90f3b6d1ba524
berrami mokhtara <berramimokhtara@gmail.com>
2023-07-10 15:59:59 -0700

    up2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,19 +9,21 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buff;
-ssize_t fd;
-ssize_t wr;
-ssize_t t;
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-{
-return (0);
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
 }
-buff = malloc(sizeof(char) * letters);
-t = read(fd, buff, lettres);
-wr = write(STDOUT_FILENO, buff, t);
-free(buff);
-close(fd);
-return (wr);
-}
+
+
