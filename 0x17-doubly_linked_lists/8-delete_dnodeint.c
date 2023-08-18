0b5c36d196bbe8385bae424bf9ff3353bf1621a0
berrami mokhtara <berramimokhtara@gmail.com>
2023-08-17 17:15:06 -0700

    mm

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,4 +1,25 @@
 #include "lists.h"
+/**
+ * dlistint_len - returns the number of nodes in a doubly linked list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
 
 /**
  * delete_dnodeint_at_index - deltes a node in a doubly linked list
